0fcc36a4258332a29f626a09fd9ea96e535c9a60
vikku0007 <vikasbharadwaj@younggeeks.in>
2021-03-19 06:13:59 -0400

    add p

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+
+import Home from './Home';
+
+
+export default history =>
+  combineReducers({
+    router: connectRouter(history),
+    home:Home
+  });
